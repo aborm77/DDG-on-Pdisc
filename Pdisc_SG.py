da794cee6c6bc876e16569851fd9e4974346d392
aborm77 <179957255+aborm77@users.noreply.github.com>
2024-11-12 12:07:08 -0800

    Update Pdisc_SG.py
    Made code more functional

--- Pdisc_SG.py
+++ Pdisc_SG.py
@@ -42,8 +42,11 @@ def angle(w1,w2):
 
 
 # given boundary data on two rays creates a Chebyshev net on the Poincare disk
+# sol_grid stores x values of pts in [:,:,0], y values in [:,:,1], and the solution
+# to the sine-Gordon equation in [:,:,2]
 def grid_solve(ax1,ax2):
     npts = ax1.shape[1]
+
     sol_grid = np.zeros((npts,npts,3))
     # Adding bndry data for R^2 mesh
     sol_grid[0,:,0] = ax1[0,:]
@@ -54,7 +57,39 @@ def grid_solve(ax1,ax2):
     sol_grid[0,:,2] = ax1[2,:]
     sol_grid[:,0,2] = ax2[2,:]
     
+    # Now creating cheby net
+    for i in range(1, npts):
+        for j in range(1, npts):
+            z2 = sol_grid[i,  j-1,:2]
+            z0 = sol_grid[i-1,j-1,:2]
+            z1 = sol_grid[i-1,j  ,:2]
+            w2 = f(z2,-z0)
+            w1 = f(z1,-z0)
+            w12 = w12_comp(w1,w2)
+            sol_grid[i,j,2] = angle(w1,w2)
+            sol_grid[i,j,:2] = f(w12,z0)
+            
+    return sol_grid
 
+# function to plot both the points on the pdisc and the rho (as a surface graph)
+def plot_grid(sol_grid, R, plots):
+    npts = sol_grid[:,:,0].shape[0]
+    if (plots=='both' or plots=='pdisc'):
+        plt.figure(1)
+        plt.scatter(sol_grid[:,:,0],sol_grid[:,:,1], alpha=0.5)
+        plt.xlim(-0.1, np.tanh(R/2)+0.1)
+        plt.ylim(-0.1, np.tanh(R/2)+0.1)
+        plt.gca().set_aspect('equal')
+    if (plots=='both' or plots=='sg'):
+        plt.figure(2)
+        x = np.linspace(0,R0,npts)
+        y = np.linspace(0,R0,npts)
+        x, y = np.meshgrid(x,y)
+        ax = plt.axes(projection='3d')
+        ax.plot_surface(x, y, sol_grid[:,:,2], alpha=0.5)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('rho')
 
 # Make the boundary geodesics
 npts = 50
@@ -69,53 +104,13 @@ y1 = np.zeros(npts)
 ax1 = np.vstack((x1,y1))
 ax2 = np.matmul(rot(phi0), ax1)
 
+phis = np.full((1,npts), phi0)
+ax1 = np.concatenate((ax1,phis), axis=0)
+ax2 = np.concatenate((ax2,phis), axis=0)
 
-# Generating the rest of the solution on the first sector
-# sol_grid1 stores x values of pts in [:,:,0] and y values in [0,0,:]
-sol_grid = np.zeros((npts,npts,3))
+# Getting solution
+sol_grid = grid_solve(ax1, ax2)
 
-# Adding bndry data for R^2 mesh
-sol_grid[0,:,0] = ax1[0,:]
-sol_grid[0,:,1] = ax1[1,:]
-sol_grid[:,0,0] = ax2[0,:]
-sol_grid[:,0,1] = ax2[1,:]
-# Adding bndry data for rho
-sol_grid[0,:,2] = phi0
-sol_grid[:,0,2] = phi0
-
-
-
-for i in range(1, npts):
-    for j in range(1, npts):
-        z2 = sol_grid[i,  j-1,:2]
-        z0 = sol_grid[i-1,j-1,:2]
-        z1 = sol_grid[i-1,j  ,:2]
-        w2 = f(z2,-z0)
-        w1 = f(z1,-z0)
-        w12 = w12_comp(w1,w2)
-        sol_grid[i,j,2] = angle(w1,w2)
-        sol_grid[i,j,:2] = f(w12,z0)
-        
-
-
-plt.figure(1)
-plt.scatter(sol_grid[:,:,0],sol_grid[:,:,1], alpha=0.5)
-plt.xlim(-0.1, np.tanh(R0/2)+0.1)
-plt.ylim(-0.1, np.tanh(R0/2)+0.1)
-plt.gca().set_aspect('equal')
-
-plt.figure(2)
-x = np.linspace(0,R0,npts)
-y = np.linspace(0,R0,npts)
-x, y = np.meshgrid(x,y)
-
-ax = plt.axes(projection='3d')
-ax.plot_surface(x, y, sol_grid[:,:,2], alpha=0.5)
- 
-# Set plot properties
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('rho')
-# ax.view_init(30, -10)
+plot_grid(sol_grid, R0, 'both')
 
 
