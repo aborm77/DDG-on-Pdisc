deleted test, fixed numerical explorations

numerical_explorations/Playing_w_Params.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov  3 11:22:20 2024
+Parameter sweep scripts for the bp1 branch point algorithm.
 
-@author: Ari Bormanis
-Purpose: DDG solving of the Sine-Gordon equation on the Poincare disk
+Generates figure sequences by sweeping over radius, separation, initial angle,
+and cutoff. Run from the repo root:
+
+    python numerical_explorations/Playing_w_Params.py
+
+Author: Ari Bormanis
 """
 
-import numpy as np
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-import Pdisc_SG_vis as vis
-from Pdisc_SG_classes import Sol_tree
+import numpy as np
+import vis
+from classes import Sol_tree
 import gc
 
-        
-    
-        
 
 if __name__ == '__main__':
     rad = False
@@ -22,64 +26,45 @@ if __name__ == '__main__':
     angle = False
     cutoff = True
 
-    # Playing with a changing radius
     if rad:
         step = 0.1
         for i in range(41):
-            jeff = Sol_tree(np.pi/2, 2.5, np.round(1 + i*step,1), 0.1)
+            jeff = Sol_tree(np.pi/2, 2.5, np.round(1 + i*step, 1), 0.1)
             jeff.bp1()
-            # ploting with just base points
-            vis.plot_grid_pdisc(jeff.base, save=True, f_name='rad/fig'+str(i))
-            # plotting branch points as well
-            vis.plot_grid_pdisc(jeff.base, plt_bps=True, save=True, f_name='rad_bps/fig'+str(i))
-            # plotting branch points and sector boundries
-            vis.plot_grid_pdisc(jeff.base, plt_bps=True, plt_bds=True, save=True, f_name='rad_bps_bndry/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=False, plt_bds=False, save=True, f_name='rad/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=True, plt_bds=False, save=True, f_name='rad_bps/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=True, plt_bds=True, save=True, f_name='rad_bps_bndry/fig'+str(i))
             del jeff
             gc.collect()
-            
-        
-    # Playing with changing sepearation
+
     if sep:
         for i in range(41):
-            jeff = Sol_tree(np.pi/2, 2.5, 3, 3 / (i+10) )
+            jeff = Sol_tree(np.pi/2, 2.5, 3, 3 / (i+10))
             jeff.bp1()
-            # ploting with just base points
-            vis.plot_grid_pdisc(jeff.base, save=True, f_name='sep/fig'+str(i))
-            # plotting branch points as well
-            vis.plot_grid_pdisc(jeff.base, plt_bps=True, save=True, f_name='sep_bps/fig'+str(i))
-            # plotting branch points and sector boundries
-            vis.plot_grid_pdisc(jeff.base, plt_bps=True, plt_bds=True, save=True, f_name='sep_bps_bndry/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=False, plt_bds=False, save=True, f_name='sep/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=True, plt_bds=False, save=True, f_name='sep_bps/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=True, plt_bds=True, save=True, f_name='sep_bps_bndry/fig'+str(i))
             del jeff
             gc.collect()
-    
-    # Playing with the initial angle
+
     if angle:
         step = ((3 * np.pi)/8) / 40
         for i in range(41):
-            jeff = Sol_tree(np.pi/2 - (i * step), 2.5, 3, 3 / 40 )
+            jeff = Sol_tree(np.pi/2 - (i * step), 2.5, 3, 3 / 40)
             jeff.bp1()
-            # ploting with just base points
-            vis.plot_grid_pdisc(jeff.base, save=True, f_name='angle/fig'+str(i))
-            # plotting branch points as well
-            vis.plot_grid_pdisc(jeff.base, plt_bps=True, save=True, f_name='angle_bps/fig'+str(i))
-            # # plotting branch points and sector boundries
-            vis.plot_grid_pdisc(jeff.base, plt_bps=True, plt_bds=True, save=True, f_name='angle_bps_bndry/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=False, plt_bds=False, save=True, f_name='angle/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=True, plt_bds=False, save=True, f_name='angle_bps/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=True, plt_bds=True, save=True, f_name='angle_bps_bndry/fig'+str(i))
             del jeff
             gc.collect()
-            
-    # Playing with the cutoff angle
+
     if cutoff:
         step = (3 - 1.7) / 40
         for i in range(41):
-            jeff = Sol_tree(np.pi/2, 1.7 + (i*step), 3, 3 / 40 )
+            jeff = Sol_tree(np.pi/2, 1.7 + (i*step), 3, 3 / 40)
             jeff.bp1()
-            # ploting with just base points
-            vis.plot_grid_pdisc(jeff.base, save=True, f_name='cut/fig'+str(i))
-            # plotting branch points as well
-            vis.plot_grid_pdisc(jeff.base, plt_bps=True, save=True, f_name='cut_bps/fig'+str(i))
-            # plotting branch points and sector boundries
-            vis.plot_grid_pdisc(jeff.base, plt_bps=True, plt_bds=True, save=True, f_name='cut_bps_bndry/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=False, plt_bds=False, save=True, f_name='cut/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=True, plt_bds=False, save=True, f_name='cut_bps/fig'+str(i))
+            vis.Pdisc_plot(jeff.base, plt_bps=True, plt_bds=True, save=True, f_name='cut_bps_bndry/fig'+str(i))
             del jeff
             gc.collect()
-                
-            

numerical_explorations/Playing_w_Params_BP2.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov  3 11:22:20 2024
+Parameter sweep scripts comparing bp1 and bp2 branch point algorithms.
 
-@author: Ari Bormanis
-Purpose: DDG solving of the Sine-Gordon equation on the Poincare disk
+Generates side-by-side figure sequences by sweeping over radius, separation,
+initial angle, and cutoff. Run from the repo root:
+
+    python numerical_explorations/Playing_w_Params_BP2.py
+
+Author: Ari Bormanis
 """
 
-import numpy as np
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-import Pdisc_SG_vis as vis
-from Pdisc_SG_classes import Sol_tree
+import numpy as np
+import vis
+from classes import Sol_tree
 import gc
 
-        
-    
-        
 
 if __name__ == '__main__':
     rad = True
@@ -22,75 +26,50 @@ if __name__ == '__main__':
     angle = False
     cutoff = False
 
-    # Playing with a changing radius
     if rad:
         step = 0.1
         for i in range(41):
-            jeff1 = Sol_tree(np.pi/5, 2.5, np.round(1 + i*step,1), 0.1)
-            jeff2 = Sol_tree(np.pi/5, 2.5, np.round(1 + i*step,1), 0.1)
-            
+            jeff1 = Sol_tree(np.pi/5, 2.5, np.round(1 + i*step, 1), 0.1)
+            jeff2 = Sol_tree(np.pi/5, 2.5, np.round(1 + i*step, 1), 0.1)
             jeff1.bp1()
             jeff2.bp2(3 * np.pi/5)
-            
-            # plotting branch points and sector boundries
-            vis.plot_grid_pdisc(jeff1.base, plt_bps=True, plt_bds=True, save=True, f_name='rad_bp1/fig'+str(i))
-            vis.plot_grid_pdisc(jeff2.base, plt_bps=True, plt_bds=True, save=True, f_name='rad_bp2/fig'+str(i))
-            
-            del jeff1
-            del jeff2
+            vis.Pdisc_plot(jeff1.base, save=True, f_name='rad_bp1/fig'+str(i))
+            vis.Pdisc_plot(jeff2.base, save=True, f_name='rad_bp2/fig'+str(i))
+            del jeff1, jeff2
             gc.collect()
-            
-        
-    # Playing with changing sepearation
+
     if sep:
         for i in range(41):
-            jeff1 = Sol_tree(np.pi/5, 2.5, 3, 3 / (i+10) )
-            jeff2 = Sol_tree(np.pi/5, 2.5, 3, 3 / (i+10) )
-            
+            jeff1 = Sol_tree(np.pi/5, 2.5, 3, 3 / (i+10))
+            jeff2 = Sol_tree(np.pi/5, 2.5, 3, 3 / (i+10))
             jeff1.bp1()
             jeff2.bp2(3 * np.pi/5)
-            
-            # plotting branch points and sector boundries
-            vis.plot_grid_pdisc(jeff1.base, plt_bps=True, plt_bds=True, save=True, f_name='sep_bp1/fig'+str(i))
-            vis.plot_grid_pdisc(jeff2.base, plt_bps=True, plt_bds=True, save=True, f_name='sep_bp2/fig'+str(i))
-            
-            del jeff1
-            del jeff2
+            vis.Pdisc_plot(jeff1.base, save=True, f_name='sep_bp1/fig'+str(i))
+            vis.Pdisc_plot(jeff2.base, save=True, f_name='sep_bp2/fig'+str(i))
+            del jeff1, jeff2
             gc.collect()
-    
-    # Playing with the initial angle
+
     if angle:
         step = ((3 * np.pi)/16) / 40
         for i in range(41):
-            jeff1 = Sol_tree(np.pi/4 - (i * step), 2.5, 3, 3 / 40 )
-            jeff2 = Sol_tree(np.pi/4 - (i * step), 2.5, 3, 3 / 40 )
-            
+            jeff1 = Sol_tree(np.pi/4 - (i * step), 2.5, 3, 3 / 40)
+            jeff2 = Sol_tree(np.pi/4 - (i * step), 2.5, 3, 3 / 40)
             jeff1.bp1()
             jeff2.bp2(3 * np.pi/5)
-  
-            # plotting branch points and sector boundries
-            vis.plot_grid_pdisc(jeff1.base, plt_bps=True, plt_bds=True, save=True, f_name='angle_bp1/fig'+str(i))
-            vis.plot_grid_pdisc(jeff2.base, plt_bps=True, plt_bds=True, save=True, f_name='angle_bp2/fig'+str(i))
-            
-            del jeff1
-            del jeff2
+            vis.Pdisc_plot(jeff1.base, save=True, f_name='angle_bp1/fig'+str(i))
+            vis.Pdisc_plot(jeff2.base, save=True, f_name='angle_bp2/fig'+str(i))
+            del jeff1, jeff2
             gc.collect()
-            
-    # Playing with the cutoff angle and rho target
+
     if cutoff:
         step1 = (3 - 1.7) / 40
         step2 = 2 / 40
         for i in range(41):
-            jeff1 = Sol_tree(np.pi/5, 1.7 + (i*step1), 3, 3 / 40 )
-            jeff2 = Sol_tree(np.pi/5, 2, 3, 3 / 40 )
-            
+            jeff1 = Sol_tree(np.pi/5, 1.7 + (i*step1), 3, 3 / 40)
+            jeff2 = Sol_tree(np.pi/5, 2, 3, 3 / 40)
             jeff1.bp1()
             jeff2.bp2(np.pi/5 * (1 + i * step2))
-            
-            # plotting branch points and sector boundries
-            vis.plot_grid_pdisc(jeff1.base, plt_bps=True, plt_bds=True, save=True, f_name='cut_bp1/fig'+str(i))
-            vis.plot_grid_pdisc(jeff2.base, plt_bps=True, plt_bds=True, save=True, f_name='cut_bp2/fig'+str(i))
-            
-            del jeff1
-            del jeff2
+            vis.Pdisc_plot(jeff1.base, save=True, f_name='cut_bp1/fig'+str(i))
+            vis.Pdisc_plot(jeff2.base, save=True, f_name='cut_bp2/fig'+str(i))
+            del jeff1, jeff2
             gc.collect()