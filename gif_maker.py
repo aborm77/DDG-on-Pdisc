a149690b136bf6a7c4404d0a7c8d0d7ac0d84537
aborm77 <179957255+aborm77@users.noreply.github.com>
2026-05-09 23:34:21 -0700

    Added cool gif functionality to \numerical_explorations via gif_grow_and_rotate.py

--- gif_maker.py
+++ gif_maker.py
@@ -7,14 +7,21 @@ Purpose: Create a gif from images
 import imageio.v3 as iio
 import os
 
-# im_dir uses Windows path separators; change to 'figs/cut_bp1/' on Mac/Linux
-im_dir = 'figs\\cut_bp1\\'
-gif_name = 'Change_cut_bp1'
-dur = 135
 
-_, _, files = next(os.walk(im_dir))
-file_count = len(files)
-images = []
-for i in range(file_count):
-  images.append(iio.imread(im_dir + 'fig'+ str(i)+ '.png'))
-iio.imwrite('gifs//'+gif_name+'.gif', images, duration = dur, loop = 0)
+def make_gif(im_dir, gif_name, dur=135, gif_dir='gifs'):
+    """Assemble all fig*.png files in im_dir into a gif saved to gif_dir/."""
+    _, _, files = next(os.walk(im_dir))
+    file_count = len(files)
+    images = []
+    for i in range(file_count):
+        images.append(iio.imread(os.path.join(im_dir, f'fig{i}.png')))
+    os.makedirs(gif_dir, exist_ok=True)
+    iio.imwrite(os.path.join(gif_dir, f'{gif_name}.gif'), images, duration=dur, loop=0)
+
+
+if __name__ == '__main__':
+    # im_dir uses Windows path separators; change to 'figs/cut_bp1/' on Mac/Linux
+    im_dir = 'figs\\cut_bp1\\'
+    gif_name = 'Change_cut_bp1'
+    dur = 135
+    make_gif(im_dir, gif_name, dur)
--- vis.py
+++ vis.py
@@ -340,6 +340,8 @@ class Surf_plot:
             
         self.pl.add_axes()
         self.pl.show()
+        
+        print(self.pl.camera_position)
 
 
     def plot_bps(self):
